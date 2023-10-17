82107ad468f2afc979bff4c9838fe8f252337582
Bikathi Martin <bikathimartin@outlook.com>
2023-10-17 15:21:05 +0300

    First commit

---
+++ src/main/java/org/xgamerstechnologies/com/XgamertechnologiesApplication.java
@@ -0,0 +1,13 @@
+package org.xgamerstechnologies.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XgamertechnologiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XgamertechnologiesApplication.class, args);
+	}
+
+}
