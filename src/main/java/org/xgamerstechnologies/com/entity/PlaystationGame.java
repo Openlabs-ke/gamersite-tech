5fdaca9486f65aa048752fcbcd3326d8b09d8251
Bikathi Martin <bikathimartin@outlook.com>
2023-10-20 23:09:55 +0300

    Mark entities as Serializable

--- src/main/java/org/xgamerstechnologies/com/entity/PlaystationGame.java
+++ src/main/java/org/xgamerstechnologies/com/entity/PlaystationGame.java
@@ -5,6 +5,9 @@ import org.xgamerstechnologies.com.type.Game;
 import org.xgamerstechnologies.com.type.enums.GamePlatform;
 
 import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,7 +17,11 @@ import javax.persistence.*;
 @Builder
 @Entity
 @Table(name = "playstation_games")
-public class PlaystationGame extends Game {
+public class PlaystationGame extends Game implements Serializable {
+    @Serial
+    @Transient
+    private static final long serialVersionUID = UUID.randomUUID().getLeastSignificantBits();
+
     @Enumerated(EnumType.ORDINAL)
     @Column(name = "game_platform")
     private GamePlatform gamePlatform;
