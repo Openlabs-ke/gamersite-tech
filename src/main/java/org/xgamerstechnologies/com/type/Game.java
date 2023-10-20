b8581a31f1f046b16d9f329995c2ea0e21b92d1c
Bikathi Martin <bikathimartin@outlook.com>
2023-10-20 22:49:11 +0300

    Rename file for better ROP

--- src/main/java/org/xgamerstechnologies/com/type/Game.java
+++ src/main/java/org/xgamerstechnologies/com/type/Game.java
@@ -1,11 +1,19 @@
 package org.xgamerstechnologies.com.type;
 
 import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @MappedSuperclass
-public class GenericGame {
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "game_id")
@@ -17,4 +25,15 @@ public class GenericGame {
 
     @Column(name = "poster_link", unique = true, nullable = false)
     private String posterLink;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "date_released")
+    private LocalDate dateReleased;
+
+    @Column(name = "game_description", nullable = false)
+    @Basic
+    private String gameDescription;
+
+    @Column(name = "num_in_stock", nullable = false)
+    private Long numInStock;
 }
