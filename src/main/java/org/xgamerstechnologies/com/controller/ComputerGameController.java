ecaf5927bae732fa6ed892ed38b57e22228305de
Bikathi Martin <bikathimartin@outlook.com>
2023-10-21 11:46:57 +0300

    Implement methods to convert to and from payload

--- src/main/java/org/xgamerstechnologies/com/controller/ComputerGameController.java
+++ src/main/java/org/xgamerstechnologies/com/controller/ComputerGameController.java
@@ -10,13 +10,14 @@ import org.xgamerstechnologies.com.abstractions.BaseGameController;
 import org.xgamerstechnologies.com.abstractions.ModelConversions;
 import org.xgamerstechnologies.com.entity.ComputerGame;
 import org.xgamerstechnologies.com.payload.GamePayload;
+import org.xgamerstechnologies.com.type.Game;
 
 import java.util.List;
 
 @RestController
 @Slf4j
 @RequestMapping(value = "/api/v1/computer-game")
-public class ComputerGameController extends ModelConversions implements BaseGameController<ComputerGame> {
+public class ComputerGameController implements BaseGameController<ComputerGame>, ModelConversions<ComputerGame> {
     @Autowired
     private ModelMapper modelMapper;
 
@@ -43,5 +44,21 @@ public class ComputerGameController extends ModelConversions implements BaseGame
     @Override
     public ResponseEntity<List<ComputerGame>> getGameList(String page, String size) {
         return null;
+    }
+
+    @Override
+    public GamePayload convertToPayload(Game game) {
+        GamePayload gamePayload = modelMapper.map(game, GamePayload.class);
+
+        // TODO: add setters as needed
+        return gamePayload;
+    }
+
+    @Override
+    public ComputerGame convertToEntity(GamePayload gamePayload) {
+        ComputerGame computerGame = modelMapper.map(gamePayload, ComputerGame.class);
+
+        // TODO: add any setters here
+        return computerGame;
     }
 }
