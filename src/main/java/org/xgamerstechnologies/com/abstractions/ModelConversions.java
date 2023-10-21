ae4376184776f2357a380527d51fdc8beb21e07e
Bikathi Martin <bikathimartin@outlook.com>
2023-10-21 11:46:29 +0300

    Make class an interface

--- src/main/java/org/xgamerstechnologies/com/abstractions/ModelConversions.java
+++ src/main/java/org/xgamerstechnologies/com/abstractions/ModelConversions.java
@@ -3,13 +3,7 @@ package org.xgamerstechnologies.com.abstractions;
 import org.xgamerstechnologies.com.payload.GamePayload;
 import org.xgamerstechnologies.com.type.Game;
 
-public abstract class ModelConversions {
-    public GamePayload convertToPayload(Game game) {
-        // TODO: add body later
-        return null;
-    }
-    public Game convertToEntity(GamePayload gamePayload) {
-        // TODO: add body later
-        return null;
-    }
+public interface ModelConversions<T> {
+    public GamePayload convertToPayload(Game game);
+    public T convertToEntity(GamePayload gamePayload);
 }
