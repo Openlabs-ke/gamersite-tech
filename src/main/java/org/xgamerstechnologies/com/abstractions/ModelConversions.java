10aa67723d5835bb7ef69910ba01580755758935
Bikathi Martin <bikathimartin@outlook.com>
2023-10-21 11:20:33 +0300

    Create abstract class to handle MODEL-DTO conversion to-and-fro

---
+++ src/main/java/org/xgamerstechnologies/com/abstractions/ModelConversions.java
@@ -0,0 +1,15 @@
+package org.xgamerstechnologies.com.abstractions;
+
+import org.xgamerstechnologies.com.payload.GamePayload;
+import org.xgamerstechnologies.com.type.Game;
+
+public abstract class ModelConversions {
+    public GamePayload convertToPayload(Game game) {
+        // TODO: add body later
+        return null;
+    }
+    public Game convertToEntity(GamePayload gamePayload) {
+        // TODO: add body later
+        return null;
+    }
+}
