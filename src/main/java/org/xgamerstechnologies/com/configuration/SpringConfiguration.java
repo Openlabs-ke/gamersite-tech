d4d4d48c6493de5bdadc3b67f5c77c74604f5089
Bikathi Martin <bikathimartin@outlook.com>
2023-10-21 20:43:32 +0300

    Mark class as a configuration class

--- src/main/java/org/xgamerstechnologies/com/configuration/SpringConfiguration.java
+++ src/main/java/org/xgamerstechnologies/com/configuration/SpringConfiguration.java
@@ -2,7 +2,9 @@ package org.xgamerstechnologies.com.configuration;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class SpringConfiguration {
     @Bean
     public ModelMapper modelMapper() {
