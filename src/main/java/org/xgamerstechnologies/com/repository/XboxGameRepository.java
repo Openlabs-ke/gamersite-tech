b574be8510a0b9e503c607b2395b114e5d74ae72
Bikathi Martin <bikathimartin@outlook.com>
2023-10-22 12:30:39 +0300

    Add methods for finding a certain game by ID

--- src/main/java/org/xgamerstechnologies/com/repository/XboxGameRepository.java
+++ src/main/java/org/xgamerstechnologies/com/repository/XboxGameRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.xgamerstechnologies.com.entity.XBoxGame;
 
+import java.util.Optional;
+
 @Repository
 public interface XboxGameRepository extends JpaRepository<XBoxGame, Long> {
+    Optional<XBoxGame> findById(Long id);
 }
