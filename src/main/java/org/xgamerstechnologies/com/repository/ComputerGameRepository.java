27965a53bfe635ed29af56ace2ca9cecf8e0ce28
Bikathi Martin <bikathimartin@outlook.com>
2023-10-22 07:18:09 +0300

    Create custome method to find computer game by ID

--- src/main/java/org/xgamerstechnologies/com/repository/ComputerGameRepository.java
+++ src/main/java/org/xgamerstechnologies/com/repository/ComputerGameRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.xgamerstechnologies.com.entity.ComputerGame;
 
+import java.util.Optional;
+
 
 @Repository
 public interface ComputerGameRepository extends JpaRepository<ComputerGame, Long> {
+    Optional<ComputerGame> findById(Long id);
 }
