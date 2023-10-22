550a966cd71a2fb811c2df2fe9458cc9f01518e2
Bikathi Martin <bikathimartin@outlook.com>
2023-10-22 15:39:41 +0300

    Create the product category controller

---
+++ src/main/java/org/xgamerstechnologies/com/controller/ProductCategoryController.java
@@ -0,0 +1,52 @@
+package org.xgamerstechnologies.com.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.xgamerstechnologies.com.abstractions.item.BaseItemsController;
+import org.xgamerstechnologies.com.abstractions.item.ItemModelConversions;
+import org.xgamerstechnologies.com.entity.ProductCategory;
+
+@RestController
+@Slf4j
+@RequestMapping(value = "/api/v1/product-category")
+public class ProductCategoryController extends ItemModelConversions<ProductCategory> implements BaseItemsController<ProductCategory> {
+    @Override
+    @ResponseStatus(code = HttpStatus.OK)
+    @PostMapping(value = "/insert", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> insertItem() {
+        return null;
+    }
+
+    @Override
+    @ResponseStatus(code = HttpStatus.OK)
+    @PostMapping(value = "/update/{categoryId}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> updateItem(@PathVariable Long categoryId) {
+        return null;
+    }
+
+    @Override
+    @ResponseStatus(code = HttpStatus.OK)
+    @DeleteMapping(value = "/delete/{categoryId}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> deleteItem(@PathVariable Long categoryId) {
+        return null;
+    }
+
+    @Override
+    @ResponseStatus(code = HttpStatus.OK)
+    @DeleteMapping(value = "/get/{categoryId}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getItem(@PathVariable Long categoryId) {
+        return null;
+    }
+
+    @Override
+    @ResponseStatus(code = HttpStatus.OK)
+    @GetMapping(value = "/get-list", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getItemList(
+            @RequestParam(defaultValue = "0") String page,
+            @RequestParam(required = false, defaultValue = "10") String size) {
+        return null;
+    }
+}
