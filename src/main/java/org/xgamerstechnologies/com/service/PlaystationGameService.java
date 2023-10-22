a7cb97c32f000cd1c720a8e33499ff9416915de1
Bikathi Martin <bikathimartin@outlook.com>
2023-10-22 12:31:08 +0300

    Complete service layer logic for both game types

--- src/main/java/org/xgamerstechnologies/com/service/PlaystationGameService.java
+++ src/main/java/org/xgamerstechnologies/com/service/PlaystationGameService.java
@@ -1,32 +1,45 @@
 package org.xgamerstechnologies.com.service;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.xgamerstechnologies.com.abstractions.GameDeletion;
 import org.xgamerstechnologies.com.abstractions.GameInsertion;
 import org.xgamerstechnologies.com.abstractions.GameRetrieval;
 import org.xgamerstechnologies.com.abstractions.GameUpdation;
 import org.xgamerstechnologies.com.entity.PlaystationGame;
+import org.xgamerstechnologies.com.repository.PlaystationGameRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlaystationGameService extends GameRetrieval<PlaystationGame> implements GameInsertion<PlaystationGame>, GameUpdation<PlaystationGame>, GameDeletion {
+    @Autowired
+    private PlaystationGameRepository playstationGameRepository;
+
     @Override
     public void deleteGame(Long gameId) {
-
+        playstationGameRepository.deleteById(gameId);
     }
 
     @Override
     public PlaystationGame insertGame(PlaystationGame playstationGame) {
-        return null;
+        return playstationGameRepository.save(playstationGame);
     }
 
     @Override
     public PlaystationGame retrieveGame(Long id) {
-        return null;
+        Optional<PlaystationGame> playstationGame = playstationGameRepository.findById(id);
+        return playstationGame.orElse(null);
     }
 
     @Override
     public PlaystationGame updateGame(PlaystationGame playstationGame) {
-        return null;
+        return playstationGameRepository.save(playstationGame);
+    }
+
+    public List<PlaystationGame> getPagedList(int pageNumber, int pageSize) {
+        return super.retrievePagedList(pageNumber, pageSize, PlaystationGame.class);
     }
 }
--- src/main/java/org/xgamerstechnologies/com/service/XBoxGameService.java
+++ src/main/java/org/xgamerstechnologies/com/service/XBoxGameService.java
@@ -1,31 +1,44 @@
 package org.xgamerstechnologies.com.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.xgamerstechnologies.com.abstractions.GameDeletion;
 import org.xgamerstechnologies.com.abstractions.GameInsertion;
 import org.xgamerstechnologies.com.abstractions.GameRetrieval;
 import org.xgamerstechnologies.com.abstractions.GameUpdation;
 import org.xgamerstechnologies.com.entity.XBoxGame;
+import org.xgamerstechnologies.com.repository.XboxGameRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class XBoxGameService extends GameRetrieval<XBoxGame> implements GameInsertion<XBoxGame>, GameUpdation<XBoxGame>, GameDeletion {
+    @Autowired
+    private XboxGameRepository xboxGameRepository;
+
     @Override
     public void deleteGame(Long gameId) {
-
+        xboxGameRepository.deleteById(gameId);
     }
 
     @Override
     public XBoxGame insertGame(XBoxGame xboxGame) {
-        return null;
+        return xboxGameRepository.save(xboxGame);
     }
 
     @Override
     public XBoxGame retrieveGame(Long id) {
-        return null;
+        Optional<XBoxGame> xBoxGame = xboxGameRepository.findById(id);
+        return xBoxGame.orElse(null);
     }
 
     @Override
     public XBoxGame updateGame(XBoxGame xboxGame) {
-        return null;
+        return xboxGameRepository.save(xboxGame);
+    }
+
+    public List<XBoxGame> getPagedList(int pageNumber, int pageSize) {
+        return super.retrievePagedList(pageNumber, pageSize, XBoxGame.class);
     }
 }
