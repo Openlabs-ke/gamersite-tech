a155246ab1ecc6db0151b95942523a9b8595054a
Bikathi Martin <bikathimartin@outlook.com>
2023-10-22 19:36:32 +0300

    Create model mapper abstractions for ProductCategory

--- src/main/java/org/xgamerstechnologies/com/abstractions/item/ItemModelConversions.java
+++ src/main/java/org/xgamerstechnologies/com/abstractions/item/ItemModelConversions.java
@@ -1,6 +1,19 @@
 package org.xgamerstechnologies.com.abstractions.item;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.xgamerstechnologies.com.payload.ProductCategoryPayload;
 import org.xgamerstechnologies.com.type.Item;
 
 public abstract class ItemModelConversions<T extends Item> {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public ProductCategoryPayload convertProductCategoryToPayload(Item item) {
+        return modelMapper.map(item, ProductCategoryPayload.class);
+    }
+
+    public T convertProductCategoryToEntity(ProductCategoryPayload productCategoryPayload, Class<T> clazz) {
+        return modelMapper.map(productCategoryPayload, clazz);
+    }
 }
